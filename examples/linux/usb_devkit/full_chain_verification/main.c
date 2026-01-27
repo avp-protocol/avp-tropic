eaf5d336e2ffe6f34218ac949be83ea78200733e
andreondra <golasowski.o@gmail.com>
2026-01-27 12:53:25 +0100

    doc(examples/linux/full_chain): update author to copyright in file comment

--- examples/linux/usb_devkit/full_chain_verification/main.c
+++ examples/linux/usb_devkit/full_chain_verification/main.c
@@ -2,7 +2,7 @@
  * @file main.c
  * @brief Utility for dumping certificates from TROPIC01 USB Devkit for Linux. Part of the Full chain verification
  * example.
- * @author Tropic Square s.r.o.
+ * @copyright Copyright (c) 2020-2026 Tropic Square s.r.o.
  *
  * @license For the license see file LICENSE.txt file in the root directory of this source tree.
  */
