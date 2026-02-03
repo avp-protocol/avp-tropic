c9bd63c645b6cf2002719783b884e5a87217004e
andreondra <golasowski.o@gmail.com>
2026-02-03 13:54:03 +0100

    scripts(codechecker): change directory handling

--- scripts/codechecker/run_checks.sh
+++ scripts/codechecker/run_checks.sh
@@ -19,35 +19,40 @@ else
     echo "Libtropic root directory set to: $LT_ROOT_DIR"
 fi
 
+mkdir -p "$LT_ROOT_DIR/.codechecker/compile_commands"
+mkdir -p "$LT_ROOT_DIR/.codechecker/reports"
+mkdir -p "$LT_ROOT_DIR/.codechecker/reports_html"
+
 # Linux USB DevKit + MbedTLSv4
 CodeChecker log -b "cd \"$LT_ROOT_DIR/examples/linux/usb_devkit/hello_world\" && rm -rf build && mkdir build && cd build && cmake .. && make -j" \
-                -o "$LT_ROOT_DIR/.codechecker/reports/usb_devkit"
+                -o "$LT_ROOT_DIR/.codechecker/compile_commands/usb_devkit_compile_commands.json"
 
 # Linux SPI + MbedTLSv4
 CodeChecker log -b "cd \"$LT_ROOT_DIR/examples/linux/spi/hello_world\" && rm -rf build && mkdir build && cd build && cmake .. && make -j" \
-                -o "$LT_ROOT_DIR/.codechecker/reports/linux_spi"
+                -o "$LT_ROOT_DIR/.codechecker/compile_commands/linux_spi_compile_commands.json"
 
 # Model + all CALs
 CALS=("trezor_crypto" "mbedtls_v4" "openssl" "wolfcrypt")
 for CURRENT_CAL in ${CALS[@]}; do
     CodeChecker log -b "cd \"$LT_ROOT_DIR/tests/functional/model\" && rm -rf build && mkdir build && cd build && cmake -DLT_CAL=$CURRENT_CAL .. && make -j" \
-                    -o "$LT_ROOT_DIR/.codechecker/reports/model"
+                    -o "$LT_ROOT_DIR/.codechecker/compile_commands/model_${CURRENT_CAL}_compile_commands.json"
 done
 
 # Merge compile_commands.json files
-jq -s 'add' "$LT_ROOT_DIR/.codechecker/reports/model" \
-            "$LT_ROOT_DIR/.codechecker/reports/usb_devkit" \
-            "$LT_ROOT_DIR/.codechecker/reports/linux_spi" \
-            > "$LT_ROOT_DIR/.codechecker/compile_commands.json"
+# Change temporarily to the directory, so we support
+# also special symbols in the path specified by $LT_ROOT_DIR.
+(cd "$LT_ROOT_DIR/.codechecker/compile_commands" && \
+    jq -s 'add' ./*_compile_commands.json \
+    > "$LT_ROOT_DIR/.codechecker/compile_commands.json")
 
 set +e
 # Run analysis on merged compilation database
 CodeChecker analyze "$LT_ROOT_DIR/.codechecker/compile_commands.json" \
                     --config "$LT_ROOT_DIR/scripts/codechecker/codechecker_config.yml" \
                     --skip "$LT_ROOT_DIR/scripts/codechecker/codechecker.skip" \
-                    -o "$LT_ROOT_DIR/.codechecker/reports/merged"
+                    -o "$LT_ROOT_DIR/.codechecker/reports"
 set -e
 
-CodeChecker parse "$LT_ROOT_DIR/.codechecker/reports/merged" \
+CodeChecker parse "$LT_ROOT_DIR/.codechecker/reports" \
                   -e html \
                   -o "$LT_ROOT_DIR/.codechecker/reports_html"
